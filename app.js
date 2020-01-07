6a7d4d367576f44cb6c557193664555e5ed2ff06
florian.wilk <florian.wilk@gmail.com>
2020-01-07 19:14:24 +0100

    fixing the app

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var mqtt = require('mqtt')
 var elasticsearch = require('elasticsearch');
 
-var client  = mqtt.connect('mqtt://localhost')
-var eclient = new elasticsearch.Client({
-    host: 'localhost:9200',
- //   log: 'trace',
-    apiVersion: '7.2', // use the same version of your Elasticsearch instance
-  });
 
+var client  = mqtt.connect('mqtt://localhost')
 client.on('connect', function () {
   client.subscribe('room-status', function (err) {
     if (!err) {
-      client.publish('room-status', 'Hello mqtt')
+//      client.publish('room-status', 'Hello mqtt')
     }
   })
 })
+
+
  
 var indexname="roomstatus"
 
@@ -24,35 +21,57 @@ function subscribe() {
         try {
           var dat = JSON.parse(message.toString())
           dat.time=Date.now()
-          console.log(dat)
+          //console.log(dat)
       
           eclient.index({  
               index: indexname,
               type: '_doc',
               body: dat
           },function(err,resp,status) {
-              console.log(resp);
+              console.log("Error while indexing document to Elasticsearch");
           });
       } catch(e){
-          console.log(e,message.toString());
+          console.log("Error while converting MQTT Message to Elasticsearch");
       }
       
       //  client.end()
       })
 }
 
-
-eclient.indices.exists({index: indexname}, (err, res, status) => {
-    if (res) {
-        console.log('index already exists');
-        subscribe()
+function runme() {
+var eclient = new elasticsearch.Client({
+    host: 'localhost:9200',
+ //   log: 'trace',
+    apiVersion: '7.2', // use the same version of your Elasticsearch instance
+  });
+eclient.ping({
+    requestTimeout: 30000
+  }, function (error) {
+    if (error) {
+//      console.err('elasticsearch cluster is down!');
+        console.log("ElasticSearch not ready yet. Will retry in 10secs....")
+        setTimeout(runme,10000)
     } else {
-        eclient.indices.create( {index: indexname}, (err, res, status) => {
-            putMapping()
-        console.log(err, res, status);
-    })
-  }
-})
+        eclient.indices.exists({index: indexname}, (err, res, status) => {
+            if(err){
+                console.err("Error while checking ElasticSearch index")
+            }
+            else
+            if (res) {
+                console.log('index already exists');
+                subscribe()
+            } else {
+                eclient.indices.create( {index: indexname}, (err, res, status) => {
+                    putMapping()
+                console.log("***",err, res, status);
+            })
+          }
+        })
+      }
+  });
+}
+
+runme()
 
 async function putMapping () {
     console.log("Creating Mapping index");
